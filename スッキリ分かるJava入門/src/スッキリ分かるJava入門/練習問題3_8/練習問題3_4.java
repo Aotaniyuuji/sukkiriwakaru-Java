6bef67bf1c24a73e357579e60384f1f47ddaa385
AotaniYuuji <aotaniyuuji@localhost>
2023-08-21 19:26:32 +0900

    練習問題3.8の3-4

---
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門/練習問題3_8/練習問題3_4.java
@@ -0,0 +1,14 @@
+package スッキリ分かるJava入門.練習問題3_8;
+
+public class 練習問題3_4 {
+	public static void main(String[] args) {
+		boolean tenki = false;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+			System.out.println("寝ます");
+		}
+	}
+}
