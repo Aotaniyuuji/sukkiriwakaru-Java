b735a3f76c83b954cf9005b05d6054c7981d6b3a
AotaniYuuji <aotaniyuuji@localhost>
2023-08-21 19:17:52 +0900

    練習問題3.8の3-3

---
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門/練習問題3_8.java
@@ -0,0 +1,19 @@
+package スッキリ分かるJava入門;
+
+public class 練習問題3_8 {
+ public static void main(String[] args) {
+	 //	 練習問題3-3
+	 int isHungry = 0;
+	 String food = "そうめん";
+	 System.out.println("こんにちは");
+	 if (isHungry == 0) {
+		 System.out.println("お腹がいっぱいです");
+	 } else {
+		 System.out.println("はらぺこです");
+	 }
+	 if (isHungry == 1) {
+		 System.out.println(food + "をいただきます");
+	 }
+	 System.out.println("ごちそうさまでした");
+ }
+}
