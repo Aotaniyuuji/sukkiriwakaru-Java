1aeb4a7674116c8e2fa41404bb00cd10e3ef051f
AotaniYuuji <Aotani1998@gmail.com>
2023-08-23 22:34:49 +0900

    配列内の要素の数を確認する方法

--- スッキリ分かるJava入門/src/スッキリ分かるJava入門第4章/配列.java
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門第4章/配列.java
@@ -2,16 +2,11 @@ package スッキリ分かるJava入門第4章;
 
 public class 配列 {
  public static void main(String[] args) {
-	 int sansu = 20;
-	 int kokugo = 30;
-	 int rika = 40;
-	 int eigo = 50;
-	 int syakai = 80;
-	 
-	 int sum = sansu + kokugo + rika + eigo + syakai;
-	 
-	 int avg = sum/ 5;
-	 System.out.println("合計点" + sum);
-	 System.out.println("平均点" + avg);
+	 int [] scores;
+	 scores = new int[5];
+	 int num = scores.length;
+	 System.out.println("要素の数:" + num);
+	 scores[1] = 30;
+	 System.out.println(scores[1]);
  }
 }
