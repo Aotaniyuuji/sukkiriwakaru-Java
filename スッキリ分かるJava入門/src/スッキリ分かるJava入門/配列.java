2e2b7d7b8ba238cbd62575a2753e78fd8b1977d0
AotaniYuuji <Aotani1998@gmail.com>
2023-08-23 21:51:59 +0900

    ファイルの位置変更

---
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門/配列.java
@@ -0,0 +1,17 @@
+package スッキリ分かるJava入門;
+
+public class 配列 {
+ public static void main(String[] args) {
+	 int sansu = 20;
+	 int kokugo = 30;
+	 int rika = 40;
+	 int eigo = 50;
+	 int syakai = 80;
+	 
+	 int sum = sansu + kokugo + rika + eigo + syakai;
+	 
+	 int avg = sum/ 5;
+	 System.out.println("合計点" + sum);
+	 System.out.println("平均点" + avg);
+ }
+}
