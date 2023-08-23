96ed94b3d62fd3e1ed1ab4010fb9419e95ad38b8
AotaniYuuji <Aotani1998@gmail.com>
2023-08-23 22:49:28 +0900

    for文を利用した配列の活用

--- スッキリ分かるJava入門/src/スッキリ分かるJava入門第4章/配列.java
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門第4章/配列.java
@@ -2,13 +2,9 @@ package スッキリ分かるJava入門第4章;
 
 public class 配列 {
  public static void main(String[] args) {
-	 int [] scores;
-	 scores = new int[5];
-	 int num = scores.length;
-	 System.out.println("要素の数:" + num);
-	 scores[1] = 30;
-	 System.out.println(scores[1]);
-	 int[] scores1 = new int[] {20,30,40,50,80};
-	 int[] scores2 = {20,30,40,50,80};
+	 int [] scores = {20,30,40,50,80};
+	 for (int i = 0; i < scores.length; i++) {
+		 System.out.println(scores[i]);
+	 }
  }
 }
