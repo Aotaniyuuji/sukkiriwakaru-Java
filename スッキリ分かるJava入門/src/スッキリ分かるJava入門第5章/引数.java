f68178935b0ff917337015ef33f01f11ad2de80d
AotaniYuuji <Aotani1998@gmail.com>
2023-08-24 17:53:10 +0900

    引数の利用[複数]

--- スッキリ分かるJava入門/src/スッキリ分かるJava入門第5章/引数.java
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門第5章/引数.java
@@ -2,13 +2,12 @@ package スッキリ分かるJava入門第5章;
 
 public class 引数 {
 	public static void main(String[] args) {
-		System.out.println("メソッドを呼び出します");
-		hello("湊");
-		hello("朝香");
-		hello("菅原");
-		System.out.println("メソッドの呼び出しが終わりました");
+		add(100, 20);
+		add(200, 50);
 	}
-	public static void hello(String name) {
-		System.out.println(name + "さん、こんにちは");
+	//複数の値を受け取るメソッド
+	public static void add(int x, int y) {
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);
 	}
 }
