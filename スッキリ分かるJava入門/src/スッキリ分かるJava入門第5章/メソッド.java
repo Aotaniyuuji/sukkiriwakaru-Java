4d493c4a73a7c88165ffa2b9b1257e9bf89e7a6e
AotaniYuuji <Aotani1998@gmail.com>
2023-08-24 17:19:27 +0900

    メソッドの定義から呼び出しまで

---
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門第5章/メソッド.java
@@ -0,0 +1,12 @@
+package スッキリ分かるJava入門第5章;
+
+public class メソッド {
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出します");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
+	public static void hello() {
+		System.out.println("湊さん、こんにちは");
+	}
+}
