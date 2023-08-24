5365fca16a382999bdb5a1f3adf2f176b794ff10
AotaniYuuji <Aotani1998@gmail.com>
2023-08-24 15:55:12 +0900

    ガベージコレクションとnullの代入

---
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門第4章/ガベージコレクション.java
@@ -0,0 +1,17 @@
+package スッキリ分かるJava入門第4章;
+
+public class ガベージコレクション {
+ public static void main(String[] args) {
+	 boolean judge = true;
+	 if (judge == true) {
+		 //配列変数自体はif文内で寿命を迎えるが、値は寿命を迎えずそのままどの配列変数からも参照できないまま残ってしまう
+		//Javaにはガベージコレクションという、そういったものを自動的に探し出して片付けてくれる仕組みが常に動いている
+		 int[] array = {1, 2, 3};
+	 }
+	 
+	 //nullを使用すると意図的に配列を参照されないようにする事ができる
+	 int[] array = {1, 2, 3};
+	 array = null;
+	 array[0] =10;
+ }
+}
--- スッキリ分かるJava入門/src/スッキリ分かるJava入門第4章/配列の舞台裏.java
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門第4章/配列の舞台裏.java
@@ -2,6 +2,7 @@ package スッキリ分かるJava入門第4章;
 
 public class 配列の舞台裏 {
  public static void main(String[] args) {
+	 //メモリと変数
 	 int [] arrayA = {1, 2, 3};
 	 int [] arrayB = arrayA;
 	 arrayB[0] = 100;
