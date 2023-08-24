86d398e4ee3c842b5a301a68dbb106ecdef920ee
AotaniYuuji <Aotani1998@gmail.com>
2023-08-24 15:33:15 +0900

    1ではなく100が表示される

---
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門第4章/配列の舞台裏.java
@@ -0,0 +1,10 @@
+package スッキリ分かるJava入門第4章;
+
+public class 配列の舞台裏 {
+ public static void main(String[] args) {
+	 int [] arrayA = {1, 2, 3};
+	 int [] arrayB = arrayA;
+	 arrayB[0] = 100;
+	 System.out.println(arrayA[0]);
+ }
+}
