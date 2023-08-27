5f9d77cc9a2fc8859477b67a4d93ea3181a3bdf3
AotaniYuuji <Aotani1998@gmail.com>
2023-08-28 00:15:04 +0900

    戻り値に配列を利用

--- スッキリ分かるJava入門/src/スッキリ分かるJava入門第5章/引数に配列の利用.java
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門第5章/引数に配列の利用.java
@@ -1,17 +1,16 @@
 package スッキリ分かるJava入門第5章;
 
 public class 引数に配列の利用 {
-	//int型配列を受け取り、
-	//配列内の全てに１を加えるメソッド
-	public static void incArray(int[] array) {
-		for (int i = 0; i < array.length; i++) {
-			array[i]++;
+	public static int[] makeArray(int size) {
+		int[] newArray = new int[size];  //メインメソッド内の実引数をint [size]で受け取り、それをさらにnewArrayに代入
+		for (int i = 0; i < newArray.length; i++) {
+			newArray[i] = i;
 		}
- 	}
+		return newArray;
+	}
 	
 	public static void main(String[] args) {
-		int[] array = {1,2,3};
-		incArray(array);
+		int[] array = makeArray(3);
 		for (int i : array) {
 			System.out.println(i);
 		}
