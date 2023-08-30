731722348f04993fb36c501364048e69153f0383
AotaniYuuji <Aotani1998@gmail.com>
2023-08-30 19:37:53 +0900

    インポートを利用した省略と注意点

--- スッキリ分かるJava入門/src/スッキリ分かるJava入門第6章/ソースファイルの分割/ソースファイルの分割.java
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門第6章/ソースファイルの分割/ソースファイルの分割.java
@@ -1,10 +1,14 @@
 package スッキリ分かるJava入門第6章.ソースファイルの分割;
 
+import スッキリ分かるJava入門第6章.計算処理メソッド.計算処理メソッド;  
+//スッキリ分かるJava入門第6章.計算処理メソッドパッケージに属する全てのクラスをインポートしたい場合は最後に[.*]をつける事で可能
+//例えばcalc.mainとcalc.logicsは全く別のパッケージ扱いなので、両方インポートする場合はimport calc.mainとcalc.logics両方記述しなければならない
+
 public class ソースファイルの分割 {
  public static void main(String[] args) {
 	 int a = 10; int b = 2;
-	 int total = スッキリ分かるJava入門第6章.計算処理メソッド.計算処理メソッド.tasu(a, b);
-	 int delta = スッキリ分かるJava入門第6章.計算処理メソッド.計算処理メソッド.hiku(a, b);
+	 int total = 計算処理メソッド.tasu(a, b);
+	 int delta = 計算処理メソッド.hiku(a, b);
 	 System.out.println("足すと" + total + "、引くと" + delta);
  }
 }
