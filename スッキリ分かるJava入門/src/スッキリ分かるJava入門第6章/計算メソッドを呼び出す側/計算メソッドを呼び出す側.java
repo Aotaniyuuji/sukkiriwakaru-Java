243027246a304fe0bb0db8a74bafc26c57af21b4
AotaniYuuji <Aotani1998@gmail.com>
2023-09-01 23:07:24 +0900

    importを利用したメソッドの呼び出し

--- スッキリ分かるJava入門/src/スッキリ分かるJava入門第6章/計算メソッドを呼び出す側/計算メソッドを呼び出す側.java
+++ スッキリ分かるJava入門/src/スッキリ分かるJava入門第6章/計算メソッドを呼び出す側/計算メソッドを呼び出す側.java
@@ -1,10 +1,10 @@
-package スッキリ分かるJava入門第6章.ソースファイルの分割;
+package スッキリ分かるJava入門第6章.計算メソッドを呼び出す側;
 
 import スッキリ分かるJava入門第6章.計算処理メソッド.計算処理メソッド;  
 //スッキリ分かるJava入門第6章.計算処理メソッドパッケージに属する全てのクラスをインポートしたい場合は最後に[.*]をつける事で可能
 //例えばcalc.mainとcalc.logicsは全く別のパッケージ扱いなので、両方インポートする場合はimport calc.mainとcalc.logics両方記述しなければならない
 
-public class ソースファイルの分割 {
+public class 計算メソッドを呼び出す側 {
  public static void main(String[] args) {
 	 int a = 10; int b = 2;
 	 int total = 計算処理メソッド.tasu(a, b);
